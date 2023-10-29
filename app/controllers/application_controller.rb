8843ed1b91413833ff7464f4ed5e20ade80e817b
Matvey Vasilyev <hounsou734@gmail.com>
2023-10-29 19:39:37 +0300

    CanCan::AccessDenied exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+    rescue_from CanCan::AccessDenied do |exception|
+        respond_to do |format|
+          format.json { head :forbidden }
+          format.html { redirect_to root_path, notice: exception.message }
+        end
+      end
 end
